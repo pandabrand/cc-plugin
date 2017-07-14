ca882aea2d428deb0a694633353f7111bab3c85d
pandabrand <pandabrand@gmail.com>
2017-07-14 00:36:05 -0500

    I do not understand the double quote issues

--- cc-plugin.php
+++ cc-plugin.php
@@ -148,10 +148,10 @@ function cc_addon_import_function($post_id, $data, $import_options, $article) {
   ));
   if(!empty($city_posts)) {
     $city_to_add = $city_posts[0];
-    $cc_addon->log( '- Adding city to location by ID: ' . $city_to_add['ID'] );
-    update_field('location_city', $city_to_add['ID']);
+    $cc_addon->log( "- Adding city to location by ID: " . $city_to_add['ID'] );
+    update_field("location_city", $city_to_add['ID']);
     //next level set the return relatioship
-    $cc_addon->log( '- Adding return reference of location to city by ID: ' . $post_id );
-    update_field('locations', $post_id, $city_to_add['ID']);
+    $cc_addon->log( "- Adding return reference of location to city by ID: " . $post_id );
+    update_field("locations", $post_id, $city_to_add['ID']);
   }
 }
