12c26b9ac75a8e259825662c9b26532b16793fde
pandabrand <pandabrand@gmail.com>
2017-07-14 01:02:20 -0500

    tryign to debug

--- cc-plugin.php
+++ cc-plugin.php
@@ -147,7 +147,8 @@ function cc_addon_import_function($post_id, $data, $import_options, $article) {
     'meta_key' => 'migrate_id',
     'meta_value' => $data['city_migrate_id']
   ));
-  write_log("city_posts retrieved : " . $city_posts[0]);
+  $r = print_r($city_posts);
+  write_log("city_posts retrieved : " . $r);
   if(!empty($city_posts)) {
     $city_to_add = $city_posts[0];
     write_log("city_posts not empty : " . $city_to_add['ID']);
