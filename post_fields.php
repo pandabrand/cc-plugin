b3582bd75f64ad7ce8064db4549768751fe53d54
pandabrand <pandabrand@gmail.com>
2017-07-17 16:19:26 -0500

    adding post field code to plugin.

---
+++ post_fields.php
@@ -0,0 +1,49 @@
+<?php
+include( plugin_dir_path( __FILE__ ).'post_fields.php' );
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_596d26c7be2fe',
+	'title' => 'Editorial Layout',
+	'fields' => array (
+		array (
+			'key' => 'field_596d26d7649bc',
+			'label' => 'Show Alternate Editorial	Layout',
+			'name' => 'show_alternate_editorial_layout',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'message' => '',
+			'default_value' => 0,
+			'ui' => 1,
+			'ui_on_text' => '',
+			'ui_off_text' => '',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
