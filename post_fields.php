34b7a63adaeeebb7de6ac054c0a9600a80616784
pandabrand <pandabrand@gmail.com>
2017-09-05 12:26:02 -0500

    adding main image position field to artist and city.

--- post_fields.php
+++ post_fields.php
@@ -254,3 +254,57 @@ acf_add_local_field_group(array (
 ));
 
 endif;
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_59aed8b026c5f',
+	'title' => 'Background Image position',
+	'fields' => array (
+		array (
+			'key' => 'field_59aedcbc1efc8',
+			'label' => 'Main Image Position',
+			'name' => 'main_image_postition',
+			'type' => 'true_false',
+			'instructions' => 'The main image will be either positioned from the top(default) of the image or the bottom.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'message' => '',
+			'default_value' => 0,
+			'ui' => 1,
+			'ui_on_text' => 'Bottom',
+			'ui_off_text' => 'Top',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'city',
+			),
+		),
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'artist',
+			),
+		),
+	),
+	'menu_order' => 5,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
