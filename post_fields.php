c8b5740bb89bb3bf0ce0c96cf96c756e1c6e1d32
pandabrand <pandabrand@gmail.com>
2017-09-26 13:43:03 -0500

    adding a field to select the desired tag.

--- post_fields.php
+++ post_fields.php
@@ -308,3 +308,53 @@ acf_add_local_field_group(array (
 ));
 
 endif;
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_59ca9e4fc2ea1',
+	'title' => 'Primary Tag Group',
+	'fields' => array (
+		array (
+			'key' => 'field_59ca9e540bd1b',
+			'label' => 'Primary tag',
+			'name' => 'primary_tag',
+			'type' => 'taxonomy',
+			'instructions' => 'Select the Tag that should be the presented tag, if none is selected the first one alphabetically will be chosen.',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'taxonomy' => 'post_tag',
+			'field_type' => 'select',
+			'allow_null' => 1,
+			'add_term' => 0,
+			'save_terms' => 0,
+			'load_terms' => 0,
+			'return_format' => 'object',
+			'multiple' => 0,
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 7,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
