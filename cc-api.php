c9c10d3f53f04b09d52440455f8825bc5e0d384c
pandabrand <pandabrand@gmail.com>
2017-11-02 12:11:46 -0500

    merge cc-api-v2 changes

---
+++ cc-api.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_cities() {
+  $cities = get_posts(
+    'post_type' => 'cities',
+  );
+
+  if( empty( $cities ) ) {
+    return null;
+  }
+
+  return $cities;
+}
+
+add_action( 'cc-rest_api_init', function () {
+  register_rest_route( 'cc-api/v1', '/cities/', array(
+    'methods' => 'GET',
+    'callback' => 'get_cities',
+  ) );
+} );
