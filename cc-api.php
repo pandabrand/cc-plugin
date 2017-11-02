eb683242fcfbce8733fb1b8ad2f327bab1f59db1
pandabrand <pandabrand@gmail.com>
2017-11-02 13:34:06 -0500

    adding location type categories api call

--- cc-api.php
+++ cc-api.php
@@ -14,9 +14,31 @@ function get_cities() {
   return $cities;
 }
 
+function get_location_categories() {
+  $args = array(
+    'name' => 'location-types'
+  );
+
+  $output = 'names';
+
+  $categories = get_taxonomies( $args, $output );
+
+  if ( empty( $categories ) ) {
+    return null;
+  }
+
+  return $categories;
+}
+
 add_action( 'rest_api_init', function () {
   register_rest_route( 'cc-api/v1', '/cities/', array(
     'methods' => 'GET',
     'callback' => 'get_cities',
   ) );
+
+  register_rest_route( 'cc-api/v1', '/categories/', array(
+    'methods' => 'GET',
+    'callback' => 'get_location_categories',
+  ) );
+
 } );
